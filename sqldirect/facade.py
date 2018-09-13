a242288d027a55f5df52d646e768f8a5ab84ab3b
Giampaolo Cimino <gcimino@gmail.com>
2018-09-13 18:57:49 +0200

    hot fix for getlstid in facades

--- sqldirect/facade.py
+++ sqldirect/facade.py
@@ -7,5 +7,5 @@ def fetchone_direct(connection, sql, types, *args, merge=None, switch=None):
 def fetchall_direct(connection, sql, types, *args, merge=None, switch=None):
     return SQLDirectConnection.create(connection).fetchall(sql, types, *args, merge=merge,  switch=switch)
 
-def execute_direct(connection, sql, *args):
+def execute_direct(connection, sql, *args, getlastid=False):
     return SQLDirectConnection.create(connection).execute(sql, *args)
