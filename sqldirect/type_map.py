117f2736116d513071e54df6678e4ccece2faa5b
Giampaolo Cimino <gcimino@gmail.com>
2018-12-05 23:48:02 +0100

    more fix for python 3.4

--- sqldirect/type_map.py
+++ sqldirect/type_map.py
@@ -63,15 +63,11 @@ class Type(object):
 
     def map(self, dbrecord):
         if self._extra_fields is not None:
-            try:
-                # this way of merge dict is valid starting from py3.5
-                dbrecord = {**dbrecord, **self._extra_fields}
-            except:
-                tmp = dbrecord.copy()
-                tmp.update(self._extra_fields)
-                dbrecord = tmp
-
-
+            # when support of py3.4 will be dropped, use the one-line below
+            # dbrecord = {**dbrecord, **self._extra_fields}
+            tmp = dict(dbrecord).copy()
+            tmp.update(self._extra_fields)
+            dbrecord = tmp
         signature = getfullargspec(self._type.__init__)
         if len(signature.args) == 2:
             return self._create1(dbrecord, signature.args)
